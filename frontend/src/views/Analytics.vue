af863af113163cf5ef181eda790f9e06286482f7
johnkntran <john.kn.tran@gmail.com>
2025-09-14 09:40:35 -0400

    Wire up Analytics page with components and composables.

--- frontend/src/views/Analytics.vue
+++ frontend/src/views/Analytics.vue
@@ -1,3 +1,17 @@
 <template>
-  You are at Analytics page.
+  <section class="container">
+    <UsernameForm @saved="userSaved"></UsernameForm>
+    <article v-if="feedbacks.length">
+      <Chart title="LLM Tokens Used" chart-id="token-chart" :feedbacks="feedbacks"></Chart>
+      <Chart title="Response Time (secs)" chart-id="latency-chart" :feedbacks="feedbacks"></Chart>
+    </article>
+  </section>
 </template>
+
+<script setup lang="ts">
+  import UsernameForm from '@/components/UsernameForm.vue';
+  import Chart from '@/components/Chart.vue';
+  import { useUserComposable } from '@/composables/userComposable.ts';
+
+  const { feedbacks, userSaved } = useUserComposable();
+</script>
